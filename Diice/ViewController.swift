f53f62923aa5c48ac3dfe6b852745e8a9be19d99
FrancaTM <francatm@outlook.com>
2020-06-18 16:35:00 -0300

    Initial Commit

---
+++ Diice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diice
+//
+//  Created by Tulio Marcos Franca on 18/06/20.
+//  Copyright © 2020 francaTM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
